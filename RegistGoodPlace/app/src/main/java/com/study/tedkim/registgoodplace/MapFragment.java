0c51439d38447dd161367bca4c436110235ceb22
Ted <kimtx2017@gmail.com>
2017-07-20 18:14:55 +0900

    Add Map Functions
    - google map ui components 추가
    - 주소 검색 기능 추가

--- RegistGoodPlace/app/src/main/java/com/study/tedkim/registgoodplace/MapFragment.java
+++ RegistGoodPlace/app/src/main/java/com/study/tedkim/registgoodplace/MapFragment.java
@@ -1,40 +1,55 @@
 package com.study.tedkim.registgoodplace;
 
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MapFragment extends Fragment implements OnMapReadyCallback {
+public class MapFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnMarkerDragListener {
 
-    private GoogleMap mGoogleMap;
+    GoogleMap mGoogleMap;
+    CameraPosition mCameraPosition;
 
     Button mNext;
+    TextView mCurrentAddr;
+
+    static final int MAX_LEN = 1;
 
     public MapFragment() {
-        // Required empty public constructor
-    }
 
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
 
         View view = inflater.inflate(R.layout.fragment_map, container, false);
 
@@ -43,16 +58,16 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         return view;
     }
 
-    private void initView(View view){
+    private void initView(View view) {
 
+        // GoogleMap Fragment 를 호출한다
         SupportMapFragment mapFragment = new SupportMapFragment();
-
         FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
-        transaction.add(R.id.fragment_googleMap, mapFragment);
+        transaction.add(R.id.layout_MapContainer, mapFragment);
         transaction.commit();
-
         mapFragment.getMapAsync(this);
 
+        // Button 동작 정의
         mNext = (Button) view.findViewById(R.id.button_next);
         mNext.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,17 +75,81 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
 
             }
         });
+
+        mCurrentAddr = (TextView) view.findViewById(R.id.textView_currentAddr);
     }
 
+    // GoogleMap 이 최초로 로딩 될때의 동작 정의
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
         mGoogleMap = googleMap;
 
-        LatLng initPosition = new LatLng(-34, 151);
-        mGoogleMap.addMarker(new MarkerOptions().position(initPosition).title("Marker In Sydney"));
-        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(initPosition));
+        // 1. 초기 맵 위치 설정
+        Bundle bundle = getArguments();
+        String address = bundle.getString("ADDRESS");
+        mCurrentAddr.setText(address);
+        try {
+
+            // 1.1 Geocoder 를 이용한 주소 검색
+            Geocoder geocoder = new Geocoder(getContext());
+            List<Address> addressList = geocoder.getFromLocationName(address, MAX_LEN);
+
+            // 1.2 유사도가 가장 높은 주소를 기반으로 '좌표 설정'
+            LatLng initPosition = new LatLng(addressList.get(0).getLatitude(), addressList.get(0).getLongitude());
+
+            // 1.3 설정 된 좌표를 기반으로 '마커 설정'
+            mGoogleMap.addMarker(new MarkerOptions().position(initPosition).draggable(true).title(address));
+            mGoogleMap.setOnMarkerDragListener(this);
+
+            // 1.4 설정 된 좌표를 기반으로 '카메라 동작 설정'
+            mCameraPosition = CameraPosition.builder().target(initPosition).zoom(14).build();
+            mGoogleMap.moveCamera(CameraUpdateFactory.newCameraPosition(mCameraPosition));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // 2. Map 에 필요한 위젯 설정
+        // 2.1 googleMap ui control 획득
+        UiSettings uiSettings = mGoogleMap.getUiSettings();
+        // 2.2 ui components 설정
+        uiSettings.setZoomControlsEnabled(true);    // +/- 버튼 활성화
+        uiSettings.setCompassEnabled(true);     // 나침반 활성화
+        uiSettings.setMapToolbarEnabled(true);  // googleMap ToolBar 활성화
+
+        // 2.3 MyLocation 버튼 활성화를 위한 self permission 체크
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+            // permission 이 허용 상태이면 MyLocation 버튼을 활성화 한다
+            mGoogleMap.setMyLocationEnabled(true);
+            uiSettings.setMyLocationButtonEnabled(true);
+        }
 
     }
 
+    // Marker 의 Drag 작업 정의
+    @Override
+    public void onMarkerDragStart(Marker marker) {
+
+    }
+
+    @Override
+    public void onMarkerDrag(Marker marker) {
+
+    }
+
+    // Marker 가 Drop 되었을 때,
+    @Override
+    public void onMarkerDragEnd(Marker marker) {
+
+        // 1. 현재 위치정보를 가져온다.
+        LatLng currentPosition = marker.getPosition();
+        mCameraPosition = CameraPosition.builder().target(currentPosition).zoom(14).build();
+        mGoogleMap.moveCamera(CameraUpdateFactory.newCameraPosition(mCameraPosition));
+
+        Log.e("CURRENT_MARKER", "Lat : "+currentPosition.latitude+" / Lng : "+currentPosition.longitude);
+
+    }
 }
--- RegistGoodPlace/app/src/main/java/com/study/tedkim/registgoodplace/RegisterFragment.java
+++ RegistGoodPlace/app/src/main/java/com/study/tedkim/registgoodplace/RegisterFragment.java
@@ -1,7 +1,11 @@
 package com.study.tedkim.registgoodplace;
 
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
@@ -10,6 +14,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 /**
@@ -21,15 +26,15 @@ public class RegisterFragment extends Fragment implements View.OnClickListener {
     EditText mShopName, mShopAddr, mShopPhone, mShopContents;
     TextView mContentsLen;
 
-    public RegisterFragment() {
-        // Required empty public constructor
-    }
+    static final int REQ_CAMERA_PERMISSION = 101;
 
+    public RegisterFragment() {
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
 
         View view = inflater.inflate(R.layout.fragment_register, container, false);
 
@@ -38,7 +43,7 @@ public class RegisterFragment extends Fragment implements View.OnClickListener {
         return view;
     }
 
-    private void initView(View view){
+    private void initView(View view) {
 
         mPrev = (Button) view.findViewById(R.id.button_prev);
         mPrev.setOnClickListener(this);
@@ -57,7 +62,7 @@ public class RegisterFragment extends Fragment implements View.OnClickListener {
     @Override
     public void onClick(View v) {
 
-        switch(v.getId()){
+        switch (v.getId()) {
 
             case R.id.button_prev:
 
@@ -65,15 +70,74 @@ public class RegisterFragment extends Fragment implements View.OnClickListener {
 
             case R.id.button_next:
 
-                setFragment(new MapFragment());
+                // Map Fragment 를 호출하기 전에 위치 정보와 관련된 Permission 들 체크
+                if (checkPermission()) {
+                    // 1. Map Fragment 호출
+                    String address = mShopAddr.getText().toString();
+                    if (!address.isEmpty()) {
+                        setFragment(new MapFragment(), address);
+                    }
+                    else {
+                        Toast.makeText(getContext(), "주소를 입력해 주셔야 합니다.", Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+                else {
+
+                    Toast.makeText(getContext(), "위치정보를 허용해 주셔야 이용 가능합니다.", Toast.LENGTH_SHORT).show();
+                }
 
                 break;
 
         }
     }
 
-    private void setFragment(Fragment fragment){
+    // 현재 위치 정보 접근을 위한 permission 체크
+    private boolean checkPermission() {
 
+        // 1. Permission 이 설정 되지 않았다면,
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            // 1.1 Permission 요청
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, REQ_CAMERA_PERMISSION);
+        }
+
+
+        return true;
+    }
+
+    // permission 요청 처리
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        // 1. 요청한 permission 들에 대해,
+        if (requestCode == REQ_CAMERA_PERMISSION) {
+            // 2. 권한을 체크한다
+            for (int grant : grantResults) {
+                // 3. ACCESS_COARSE_LOCATION, ACCESS_FINE_LOCATION 중 하나라도 허용이 되지 않으면,
+                if (grant == PackageManager.PERMISSION_DENIED) {
+
+                    // TODO - 어떤 동작을 할지 생각해 볼 것
+                    // 4. Activity 를 종료... 한다
+                    Toast.makeText(getContext(), "허용을 눌러라", Toast.LENGTH_SHORT).show();
+                    getActivity().finish();
+                }
+            }
+        }
+    }
+
+    // 주소정보를 가지고 Map Fragment 호출
+    private void setFragment(Fragment fragment, String address) {
+
+        // 1. 주소 정보 전달
+        Bundle bundle = new Bundle();
+        bundle.putString("ADDRESS", address);
+        fragment.setArguments(bundle);
+
+        // 2. Map Fragment 호출
         FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction().addToBackStack(null);
         transaction.replace(R.id.layout_container, fragment);
         transaction.commit();
