de5bcac8938787435e55ee0008e46fcbb70b3b7f
Yunkyun Han <kyunbaragi@daum.net>
2017-07-21 20:04:26 +0900

    기본 요구사항 구현

--- app/src/main/java/com/project3/mini/yunkyun/miniproject3/MainActivity.java
+++ app/src/main/java/com/project3/mini/yunkyun/miniproject3/MainActivity.java
@@ -1,5 +1,6 @@
 package com.project3.mini.yunkyun.miniproject3;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -12,16 +13,24 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocomplete;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
-    @BindView(R.id.et_name) EditText name;
-    @BindView(R.id.et_address) EditText address;
-    @BindView(R.id.et_phonenumber) EditText phoneNumber;
-    @BindView(R.id.et_memo) EditText memo;
+    private static final int PLACE_AUTOCOMPLETE_REQUEST_CODE = 1;
+    @BindView(R.id.et_name) EditText nameField;
+    @BindView(R.id.et_address) TextView addressField;
+    @BindView(R.id.et_phonenumber) EditText phoneNumberField;
+    @BindView(R.id.et_memo) EditText memoField;
     @BindView(R.id.tv_character_counter) TextView lengthCounter;
+    private Place place;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +39,12 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         setToolbar();
+        setViewListener();
 
-        memo.addTextChangedListener(new TextWatcher() {
+    }
+
+    private void setViewListener() {
+        memoField.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
             }
@@ -47,6 +60,13 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        addressField.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                searchAddress();
+            }
+        });
     }
 
     private void setToolbar() {
@@ -55,16 +75,68 @@ public class MainActivity extends AppCompatActivity {
         actionBar.setHomeButtonEnabled(true);
     }
 
-    @OnClick({R.id.btn_prev, R.id.btn_next})
+
+    private void searchAddress() {
+        try {
+            Intent intent = new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_FULLSCREEN).build(this);
+            startActivityForResult(intent, PLACE_AUTOCOMPLETE_REQUEST_CODE);
+        } catch (GooglePlayServicesRepairableException e) {
+            // TODO: Handle the error.
+        } catch (GooglePlayServicesNotAvailableException e) {
+            // TODO: Handle the error.
+        }
+    }
+
+
+    @OnClick({R.id.btn_prev, R.id.btn_main_next})
     void onButtonClick(View view) {
         switch (view.getId()) {
             case R.id.btn_prev:
                 Toast.makeText(this, getResources().getString(R.string.touch_btn_prev), Toast.LENGTH_SHORT).show();
                 break;
-            case R.id.btn_next:
+            case R.id.btn_main_next:
+                registerRestaurant();
                 break;
             default:
                 break;
+        }
+    }
+
+    private void registerRestaurant() {
+        String nameInput = nameField.getText().toString();
+        String addressInput = addressField.getText().toString();
+        String phoneNumberInput = phoneNumberField.getText().toString();
+        String memoInput = memoField.getText().toString();
+        if(nameInput.length() != 0 && addressInput.length() != 0 && phoneNumberInput.length() != 0){
+            if(place != null) {
+                double latitude = place.getLatLng().latitude;
+                double longitude = place.getLatLng().longitude;
+                Intent intent = new Intent(MainActivity.this, MapActivity.class);
+                intent.putExtra("name", nameInput);
+                intent.putExtra("address", addressInput);
+                intent.putExtra("phoneNumber", phoneNumberInput);
+                intent.putExtra("memo", memoInput);
+                intent.putExtra("latitude", latitude);
+                intent.putExtra("longitude", longitude);
+                startActivity(intent);
+            }
+        } else {
+            Toast.makeText(this, getResources().getString(R.string.alert_requirement), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_AUTOCOMPLETE_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                place = PlaceAutocomplete.getPlace(this, data);
+                addressField.setText(place.getAddress());
+            } else if (resultCode == PlaceAutocomplete.RESULT_ERROR) {
+                Status status = PlaceAutocomplete.getStatus(this, data);
+                // TODO: Handle the error.
+            } else if (resultCode == RESULT_CANCELED) {
+                // The user canceled operation.
+            }
         }
     }
 
